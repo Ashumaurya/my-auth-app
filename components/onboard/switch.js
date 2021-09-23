5ccc82d021dea1a2da02648ba3a5a24e8996dbe8
Ashish Maurya <ashish.1999vns@gmail.com>
2021-09-24 00:15:10 +0530

    layout done

---
+++ components/onboard/switch.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Switch({ isSignUp, setIsSignUp }) {
+  const [toggle, setToggle] = useState(true);
+  const toggleClass = " transform translate-x-16 relative";
+  return (
+    //   Switch Container
+    <div
+      className="w-32 h-10  flex items-center bg-gray-300 rounded-full  relative cursor-pointer"
+      onClick={() => {
+        setToggle(!toggle);
+        setIsSignUp(!isSignUp);
+      }}
+    >
+      {/* Switch */}
+      <div className="p-2 font-bold text-xs absolute">Log In</div>
+      <div
+        className={
+          "bg-black w-16 h-10 h-full w-full rounded-full shadow-md transform duration-300 ease-in-out py-4 z-10 relative" +
+          (toggle ? null : toggleClass)
+        }
+      ></div>
+      <div className="p-2 py-4 font-bold text-xs relative z-0">Sign Up</div>
+    </div>
+  );
+}
+
+export default Switch;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,41 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import SignUp from "../components/onboard/signUpform";
+import Switch from "../components/onboard/switch";
+import pic from "../public/img/login.jpg";
+import Login from "../components/onboard/logInForm";
 
-export default function Home() {
+export default function Onboard() {
+  const [isSignUp, setIsSignUp] = useState(true);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="h-screen w-full">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="overflow-x-hidden h-screen">
+        <div className=" grid lg:grid-cols-2 grid-cols-1 xl:gap-x-6 gap-x-6">
+          <div className="hidden lg:block flex my-auto">
+            <Image src={pic} alt="Mind Map Picture" layout="responsive" />
+          </div>
+          <div>
+            <div className="flex ">
+              <div className="md:p-10 md:px-20 flex flex-col max-w-8/12 flex justify-center items-center md:h-full  h-screen">
+                <div className=" border-1 shadow-xl md:px-8 md:py-4 py-2 px-4 rounded-xl h-full">
+                  <div className="my-2">
+                    <Switch isSignUp={isSignUp} setIsSignUp={setIsSignUp} />
+                  </div>
+                  <div className="my-4 h-full">
+                    {isSignUp ? <SignUp /> : <Login />}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
+  );
 }
